ff36d6a68105716ba119cf747f78c1035eb4502a
Shantanu <Shantanu@DESKTOP-AVK3V0R>
2022-08-18 23:39:48 +0530

    UML

---
+++ TNS_C2TC_Java/src/com/tns/session/UML.java
@@ -0,0 +1,25 @@
+package com.tns.session;
+
+public class UML {
+
+
+		class Car {
+		    private String color;
+		    private int weight;
+		    private Brand brand_name;
+		}
+
+		class Brand {
+		    private String Skoda;
+		    private String BMW;
+		    private Location location;
+		}
+
+		class Location {
+		    private String US;
+		    private String EU;
+		}
+
+	}
+	
+
